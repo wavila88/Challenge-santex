98f044e6e049f98d2f0836d2e66b19abeac99a28
William Avila <willocoav@example.com>
2021-10-20 22:36:06 -0500

    First commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <header>
+      <img
+        src="https://santex.wpengine.com/wp-content/uploads/2019/02/logo-santex@3x.png"
+        alt="logo"
+      />
+      <div>$ 0</div>
+    </header>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,63 @@
+import {
+  ApolloClient,
+  ApolloLink,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+const commerceLink = createHttpLink({
+  uri: 'https://shrouded-wave-86340.herokuapp.com/shop-api',
+  headers: {
+    authorization: localStorage.getItem('Auth-Token')
+      ? `Bearer ${localStorage.getItem('Auth-Token')}`
+      : '',
+  },
+});
+
+const userLink = createHttpLink({
+  uri: 'https://json-placeholder-graphql.herokuapp.com/graphql',
+});
+
+const afterwareLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map((response) => {
+    const context = operation.getContext();
+    const authHeader = context.response.headers.get('Vendure-Auth-Token');
+
+    if (authHeader) {
+      localStorage.setItem('Auth-Token', authHeader);
+    }
+    return response;
+  });
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: ApolloLink.from([
+    afterwareLink,
+    ApolloLink.split(
+      (operation) => operation.getContext().service === 'user',
+      userLink,
+      commerceLink
+    ),
+  ]),
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
